aba84e95fbd023ede389559f71050161e0dc5892
Alisahhh <923431911@qq.com>
2018-01-28 15:10:41 +0800

    updata test

--- hiredis/test.cpp
+++ hiredis/test.cpp
@@ -1,21 +1,74 @@
-#include "hiredis.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+#include <assert.h>
+#include <hiredis/hiredis.h>
+
+void doTest()
+{
+	//redis默认监听端口为6387 可以再配置文件中修改
+	redisContext* c = redisConnect("127.0.0.1", 6379);
+	if ( c->err)
+	{
+		redisFree(c);
+		printf("Connect to redisServer faile\n");
+		return ;
+	}
+	printf("Connect to redisServer Success\n");
+
+	const char* command1 = "SET stest1 value1";
+	redisReply* r = (redisReply*)redisCommand(c, command1);
+
+	if( NULL == r)
+	{
+		printf("Execut command1 failure\n");
+		redisFree(c);
+		return;
+	}
+	if( !(r->type == REDIS_REPLY_STATUS && strcasecmp(r->str,"OK")==0))
+	{
+		printf("Failed to execute command[%s]\n",command1);
+		freeReplyObject(r);
+		redisFree(c);
+		return;
+	}
+	freeReplyObject(r);
+	printf("Succeed to execute command[%s]\n", command1);
+
+	const char* command3 = "GET stest1";
+	r = (redisReply*)redisCommand(c, command3);
+	if ( r->type != REDIS_REPLY_STRING)
+	{
+		printf("Failed to execute command[%s]\n",command3);
+		freeReplyObject(r);
+		redisFree(c);
+		return;
+	}
+	printf("The value of 'stest1' is %s\n", r->str);
+	freeReplyObject(r);
+	printf("Succeed to execute command[%s]\n", command3);
+
+	const char* command4 = "GET stest2";
+	r = (redisReply*)redisCommand(c, command4);
+	if ( r->type != REDIS_REPLY_NIL)
+	{
+		printf("Failed to execute command[%s]\n",command4);
+		freeReplyObject(r);
+		redisFree(c);
+		return;
+	}
+	freeReplyObject(r);
+	printf("Succeed to execute command[%s]\n", command4);
 
 
-int main(){
-  redisContext *c = redisConnect("127.0.0.1", 6379);
-  if (c != NULL && c->err) {
-      printf("Error: %s\n", c->errstr);
-      // handle error
-    } else {
-      printf("Connected to Redis\n");
-    }
-    redisReply *reply;
-    reply = (redisReply *)redisCommand(c,"SET %s %s","foo","bar");
-    freeReplyObject(reply);
-    reply = (redisReply *)redisCommand(c,"SET %s %s","foo","bar1");
-    freeReplyObject(reply);
-    reply = (redisReply *)redisCommand(c,"GET %s","foo");
-    printf("%s\n",reply->str);
-    freeReplyObject(reply);
-    redisFree(c);
+	redisFree(c);
+
+}
+
+int main()
+{
+	doTest();
+	return 0;
 }
